eb3f59884dc51b8ddb1b89f651e02a617d4b87d2
katelynnbyoung <kbyoung@up.edu.ph>
2024-03-03 19:41:53 +0800

    Starting

--- Thesis Data Analysis.R
+++ Thesis Data Analysis.R
@@ -9,16 +9,43 @@ library("dplyr")
 library("tidyr")
 library("readxl")
 library("ggplot2")
+library(DescTools)
 
-
+# Loading data
 Data <- read_xlsx("D:/UP/Thesis/Thesis/Thesis.xlsx")
-
 print(Data, n=22)
 
+# Transforming the data to numeric class
+transform(Data, SOC = as.numeric(SOC), 
+          TN = as.numeric(TN),
+          BD = as.numeric(BD))
+
+# Separating the columns
 SOC <- Data[2]
 
 TN <- Data[3]
 
+BD <- Data[4]
+
+
+# Finding out more about each column
 SOC_df <- SOC %>%
-  summarise(ave_ratio = mean(ratio))
-  
+  summarise(average = mean(SOC),
+            median = median(SOC),
+            mode =  Mode(SOC),
+            minimum = min(SOC),
+            maximum = max(SOC))
+
+TN_df <- TN %>%
+  summarise(average = mean(TN),
+            median = median(TN),
+            mode = Mode(TN),
+            minimum = min(TN),
+            maximum = max(TN))
+
+BD_df <- BD %>%
+  summarise(average = mean(BD),
+            median = median(BD),
+            mode = Mode(BD),
+            minimum = min(BD),
+            maximum = max(BD))
